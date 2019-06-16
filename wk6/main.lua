28a5b1aa3a63c8b362a015871519e15c8544f512
Daniele Basile <asterix24@gmail.com>
2019-06-16 18:20:09 +0200

    Add blink test.

--- wk6/main.lua
+++ wk6/main.lua
@@ -8,21 +8,21 @@ display.write({0, 0, 0, 0, 0, 0, 0, 0})
 display.write({0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff})
 for x=1,2 do
   for i = 0,5 do
-    print(i)
     display.brightness(i)
     tmr.delay(1000)
   end
 end
 
-print("smile")
-c = {0x3c, 0x42 ,0xa5 ,0x81 ,0xa5 ,0x99 ,0x42 ,0x3c}
-b = {}
-j = 1
-for i=#c,1,-1 do
-  b[j] = c[i]
-  j = j + 1
-end
-tmr.delay(10 * 1000)
-display.write(b)
+local flag = true
+display.brightness(5)
+tmr.create():alarm(1000, tmr.ALARM_AUTO, function()
+  if flag then
+    display.write({0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff})
+  else
+    display.write({0, 0, 0, 0, 0, 0, 0, 0})
+  end
+  flag = not flag
+end)
+
 
 
